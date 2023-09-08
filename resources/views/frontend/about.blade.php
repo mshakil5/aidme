367e518bd51743a75d0145339d80bcb0498bea03
mshakil5 <kazimuhammadullah@gmail.com>
2023-09-08 22:22:38 +0600

    logo change

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -22,15 +22,22 @@
             </div>
             <div class="col-md-6 ">
                 <div class="about-right mt-5">
-                    {{-- <h6 class="txt-primary fs-4 d-flex align-items-center">
-                        <iconify-icon icon="ph:heart-fill"></iconify-icon>
-                        About EnaCare
-                    </h6> --}}
-                    <h2 class="title-global">{{\App\Models\Master::where('name','about')->first()->title}}</h2>
+                    {{-- <h2 class="title-global">{{\App\Models\Master::where('name','about')->first()->title}}</h2>
+                    {!! \App\Models\Master::where('name','about')->first()->description !!} --}}
+
+                    <h2 class="title-global">About us</h2>
+
+                     <p>AID ME UK is a non-profit organisation working to strengthen humanity’s fight against poverty, social injustice and natural disaster. Through the provision of immediate relief and the establishment of local community media, we aim to invest in real, effective solutions.</p>
+                    <p>We work to establish healthcare, education, and livelihood programmes that pave the way for empowered, self-serving communities. We also provide food, medical aid, and disaster relief during emergencies, a critical intervention that saves lives.</p>
                     
-
-                    {!! \App\Models\Master::where('name','about')->first()->description !!}
-
+                    <h2 class="title-global">Our mission </h2>
+                    
+                    <p>We are a humanitarian welfare organisation. We save lives, alleviate poverty, transform and empower local communities, build shelter, supply water, education, support health and medical to vulnerable people.</p>
+                    
+                    <h2 class="title-global">Our vision</h2>
+                    
+                    To become a successful worldwide organisation of change for a caring, healthy and sustainable local community.
+                    
                         
                     
                 </div>
